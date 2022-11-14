3433e13a5605a80da53b2ea024959d076120dab2
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-11-13 18:32:58 -0600

    Create playcover-prerelease.rb

---
+++ Casks/playcover-prerelease.rb
@@ -0,0 +1,24 @@
+cask "playcover-prerelease" do
+  version "2.0.0-beta.1"
+  sha256 "cd4ac2a53140c0034eeb50e62b335f1729639cbeaf43f7228c976d4d9aee7ab8"
+
+  url "https://github.com/PlayCover/PlayCover/releases/download/#{version}/PlayCover_#{version}.dmg"
+  name "PlayCover"
+  desc "Sideload iOS apps and games"
+  homepage "https://github.com/PlayCover/PlayCover"
+
+  auto_updates true
+  depends_on arch: :arm64
+  depends_on macos: ">= :monterey"
+
+  app "PlayCover.app"
+
+  zap trash: [
+    "~/Library/Application Support/io.playcover.PlayCover",
+    "~/Library/Caches/io.playcover.PlayCover",
+    "~/Library/Containers/io.playcover.PlayCover",
+    "~/Library/Frameworks/PlayTools.framework",
+    "~/Library/Preferences/io.playcover.PlayCover.plist",
+    "~/Library/Saved Application State/io.playcover.PlayCover.savedState",
+  ]
+end
