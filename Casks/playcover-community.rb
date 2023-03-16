f619bcb83dbfe69c1b36c23855f828247aae929f
Depal1 <47154119+Depal1@users.noreply.github.com>
2023-03-15 22:53:49 -0600

    Update playcover-community.rb

--- Casks/playcover-community.rb
+++ Casks/playcover-community.rb
@@ -1,6 +1,6 @@
 cask "playcover-community" do
-  version "2.0.3"
-  sha256 "af9931635bf1845535da5e4ad07f25a9eba9f61d07651a3c8e034170c9f81f01"
+  version "2.0.5"
+  sha256 "d360235072d161d5e3da96e9c0e8cf5e7e1d63042a356f02230380e61947757a"
 
   url "https://github.com/PlayCover/PlayCover/releases/download/#{version}/PlayCover_#{version}.dmg"
   name "PlayCover"
